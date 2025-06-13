1c10a60d8a0035ac11e413a85bbdd618d7d8bd01
Sato Koichiro <koichirou310@gmail.com>
2025-06-13 21:33:30 +0900

    Implement ST instruction

--- src/cpuboard.c
+++ src/cpuboard.c
@@ -6,20 +6,41 @@
  *	Descrioption:	simulation(emulation) of an instruction
  */
 
+#include <stdio.h>
 #include	"cpuboard.h"
 
 
 /*=============================================================================
  *   Simulation of a Single Instruction
  *===========================================================================*/
+enum {
+        OP_HLT = 0x00,
+        OP_STA_ABS = 0x10
+};
+
 int
 step(Cpub *cpub)
 {
-	/*
-	 *   [ add codes here for the simulation of an instruction ]
-	 *   [ return RUN_STEP or RUN_HALT ]
-	 */
-	return RUN_HALT;
+        Uword   op;
+
+        op = cpub->mem[cpub->pc++];
+
+        switch( op ) {
+           case OP_HLT:
+                return RUN_HALT;
+
+           case OP_STA_ABS: {
+                Addr addr = cpub->mem[cpub->pc++];
+                cpub->mem[0x100 | addr] = cpub->acc;
+                break;
+           }
+
+           default:
+                fprintf(stderr,"Unknown opcode: 0x%02x\n",op);
+                return RUN_HALT;
+        }
+
+        return RUN_STEP;
 }
 
 
