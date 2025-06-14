a5cea4837443cbc108f0b2f0f02d6a9f4a7e8ec2
Sato Koichiro <koichirou310@gmail.com>
2025-06-14 13:00:33 +0900

    Improve test runner to rebuild program

---
+++ tests/run_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+SCRIPT_DIR="$(dirname "$0")"
+
+# Build the project
+make clean >/dev/null
+make >/dev/null
+
+EXIT_STATUS=0
+for test_script in "$SCRIPT_DIR"/test_*.sh; do
+  echo "Running $(basename "$test_script")" >&2
+  if sh "$test_script"; then
+    echo "PASS" >&2
+  else
+    echo "FAIL" >&2
+    EXIT_STATUS=1
+  fi
+  echo >&2
+done
+exit $EXIT_STATUS
