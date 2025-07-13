a7130332139d1aa81240bafe15d56d6ae68bb4b0
koichiro.sato <koichirou310@gmail.com>
2025-07-13 09:40:11 +0900

    fix:style

--- cpu/src/cpu_board.c
+++ cpu/src/cpu_board.c
@@ -6,26 +6,22 @@
  *	Descrioption:	simulation(emulation) of an instruction
  */
 
-#include	"cpu_board.h"
-/* 命令実行サイクル */
-#include	"isa.h"
-#include   "isa_table.h"
-#include	"cpu_fetch.h"
-#include	"cpu_decode.h"
-
+#include "cpu_board.h"
+#include "isa.h"
+#include "isa_table.h"
+#include "cpu_fetch.h"
+#include "cpu_decode.h"
 
 /*=============================================================================
  *   Simulation of a Single Instruction
  *===========================================================================*/
-int
-step(Cpub *cpub)/* 1ステップ実行 */
+int step(Cpub *cpub) /* 1ステップ実行 */
 {
 	Instruction inst;
-	if (!fetch_instruction(cpub, &inst)) {
+	if (!fetch_instruction(cpub, &inst))
+	{
 		return RUN_HALT;
 	}
 	decode_instruction(cpub, &inst);
 	return isa_exec_table[inst.opcode](cpub, &inst);
 }
-
-
